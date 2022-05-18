7ae4f86c2ffbbd1f0350c4d6345c97906879a097
Fletcher Heisler <>
2022-05-18 11:19:56 -0400

    Add halfstash, width tracking

--- filter.py
+++ filter.py
@@ -1,11 +1,5 @@
 import cv2
 import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_face_mesh = mp.solutions.face_mesh
-
-
-drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
 cap = cv2.VideoCapture(2) # depends on input device, usually 0
 
@@ -14,27 +8,36 @@ def get_coords(point, image):
   y = int(image.shape[0] * point.y)
   return (x, y)
 
-def draw(face, image):
-  lip_mid = face[164]
-  x, y = get_coords(lip_mid, image)
-  width, height = 200, 50
-
-  stash = cv2.imread('mustache.png', cv2.IMREAD_UNCHANGED)
-  stash = cv2.resize(stash, (width, height))
-
-  minY = int(y - height/2)
-  maxY = int(y + height/2)
-  minX = int(x - width/2)
-  maxX = int(x + width/2)
-
+def add_halfstash(minX, maxX, minY, maxY, stash, image):
+  stash = cv2.resize(stash, (maxX - minX, maxY - minY))
   alpha = stash[:, :, 3] / 255.0
   alpha_inv = 1 - alpha
   for c in range(0, 3):
     image[minY:maxY, minX:maxX, c] = (
       alpha * stash[:,:,c] + alpha_inv * image[minY:maxY, minX:maxX, c])
 
+def draw(face, image):
+  lip_mid = face[164]
+  lip_left = face[322]
+  lip_right = face[92]
+  lip_upper = face[2]
+  lip_lower = face[0]
 
-with mp_face_mesh.FaceMesh(
+  x_mid, y_mid = get_coords(lip_mid, image)
+  x_left, y_left = get_coords(lip_left, image)
+  x_right, y_right = get_coords(lip_right, image)
+
+  _, y_upper = get_coords(lip_upper, image)
+  _, y_lower = get_coords(lip_lower, image)
+
+  stash = cv2.imread('halfstash.png', cv2.IMREAD_UNCHANGED)
+  add_halfstash(x_mid, x_left, y_upper, y_lower, stash, image)
+
+  stash = cv2.flip(stash, 1)
+  add_halfstash(x_right, x_mid, y_upper, y_lower, stash, image)
+
+
+with mp.solutions.face_mesh.FaceMesh(
     max_num_faces=1,
     refine_landmarks=True) as face_mesh:
   while cap.isOpened():
